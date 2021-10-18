8598b7e5dd3147c43ca4dd29654e242f238a41ba
Amy Davies <Amy@Scotts-MacBook-Pro-331.local>
2021-10-18 15:41:37 +0200

    notes

--- 01_code-js_datatypes.js
+++ 01_code-js_datatypes.js
@@ -1,4 +1,38 @@
 // Students code
 
-// Welcome to Ironhack Web Dev Full Time in Barcelona
-// new
+var globasNamespaceVariable = "This is global" //can go into every scope inc curly brackets 
+
+let blockScopedVariable = "I am valid in ablock"
+
+const constantAndBlockScopedVar = "I am a const"
+
+const cappucino = "Hello I am Mr. Cappucino"
+
+// cappucino = "I have been reassigned"
+
+{
+// A block is a place in the file where certain variables and functions are readabble (defined)
+    let cappucino = "I am inside a funciton"
+}
+
+const todaysdate = "18/10/2021"
+// computer just sees characters doesnt understand its a date, string defined by quotes, quotes arent a part of the string
+
+const iamyearsold = 34
+// make a choice about data types and giving a value
+
+const millionsinthebank = null
+
+const doIlikeGermany = undefined
+
+const amIEnglish = true
+const doIlikebananas = false
+// data types that signify that something is true or false - logic (boolean) go together in pairs
+
+const expressionhere = (2+5) * 6
+//value isnt here, it is an expression
+
+const powerfulstrings = `I am ${iamyearsold} years old`
+// string templetes - allow you to write string in natural language, can bring in a value with dollar and curly brackets 
+
+console.log(powerfulstrings)

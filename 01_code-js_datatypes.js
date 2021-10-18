8c912c1fdbf4aef7d2a8049049a1da10ad1fea3b
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-10-18 15:48:27 +0200

    WIP merge conflicts

--- 01_code-js_datatypes.js
+++ 01_code-js_datatypes.js
@@ -1,24 +1,30 @@
-// Students code
-
-// Welcome to Ironhack Web Dev Full Time in Barcelona
-
-// Hello Eli, nice to see you
-
-// We have done e complete git workflow
-
-// Hello Javi, nice to see you
-
-// Can you copy paste this line and change the name to Javi?
-
-// Can you copy paste this line and change the name to Javi?
-
-// Can you copy paste this line and change the name to Javi?
-// Can you copy paste this line and change the name to Julia
-
-// Can you copy paste this line and change the name to Javi?
-
-// Can you copy paste this line and change the name to Dani?
-
-// Can you copy paste this line and change the name to Eric
 
 
+var globalNamespaceVariable = "This is global"
+
+let blockScopedVariable = "I am valid in a block"
+
+const constantAndBlockScopedVar = "I am a const"
+
+const cappucino = "Hello I am Mr. Cappucino"
+
+// cappucino = "I have been reassigned"
+
+// Primitive Data Types: 
+const todaysdate = "'18/10/2021😁"
+
+const iamyearsold = 48
+
+const millionsinthebank = null
+
+const doIlikeGermany = undefined
+
+const amIItalian = true
+const doIlikePiña = false
+
+const expressionHere = (2+5) * 6
+
+// string templates
+const powerfulStrings = `I am ${iamyearsold} years old`
+
+console.log(powerfulstrings)
